3dfc2514cfa379a3e50cd538007a5b5a00df1a6a
Daniel Kerschensteiner <55744454+kerschensteinerd@users.noreply.github.com>
2026-03-09 17:16:07 -0500

    Skip addpath in deployed builds

--- setupErgAnalyzerPath.m
+++ setupErgAnalyzerPath.m
@@ -1,5 +1,12 @@
 function setupErgAnalyzerPath()
 % setupErgAnalyzerPath Add the ERG Analyzer source folders to the MATLAB path.
+%
+% In deployed builds, packaged files are already discoverable and addpath is not
+% supported by MATLAB Compiler.
+
+if isdeployed
+    return;
+end
 
 rootDir = getErgAnalyzerRoot();
 addpath(rootDir);
